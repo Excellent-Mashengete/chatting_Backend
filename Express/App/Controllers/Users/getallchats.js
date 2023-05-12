b5832bc4b0caa6c9014b74d02b710f699d519f5e
Excellent <mashengete@live.com>
2023-05-12 09:42:45 +0200

    can see last messages and all contacts chatting with

--- Express/App/Controllers/Users/getallchats.js
+++ Express/App/Controllers/Users/getallchats.js
@@ -7,34 +7,78 @@ module.exports.getallchats = async (req, res) => {
     const id = req.userId;
 
     try{
-        //get all user you are chatting to
-        const contacts = await messages.findAll({
-            where: { 
-                [Sequelize.Op.or]: [
-                    {sender: id},
-                    {receiver: id},
-                ],
-              
-            },
-            include: { 
-                model: users,
-                    attributes:[
-                    'id', 'firstname', 'lastname','email',
-                    'phone','avatar'
-                ]
-            },
-
-            group:[
-                'chat_messages.id'
+    const contacts = await messages.findAll({
+        where: {
+            [Sequelize.Op.or]:[
+                {sender: id},
+                {receiver: id}  
             ],
+        },
+        attributes: [
+            "messages", "read", "receiver", "sender",
+            [Sequelize.literal(`(chat_messages.createdAt)`), 'datesend']
+        ],
+        include: [
+            {
+                model: users,
+                attributes: ['id', 'firstname', 'phone', 'avatar']
+            },
+            {
+                model: users, as: "receipient",
+                attributes: ['id', 'firstname', 'phone', 'avatar']
+            },
+        ],
 
-            order:[
-                ['createdAt', 'DESC'],
-                [ Sequelize.fn('min', Sequelize.col('chat_messages.createdAt')),'DESC']    
-            ], 
-        });
-    
-        return res.status(200).json({ messages: "Successfully queried all users", allContact: contacts});
+        order: [
+            ['datesend', 'DESC'],
+        ],
+
+    });
+
+    const userIds = contacts.filter((m) =>{
+        m.dataValues.user.dataValues.id.toString() !== id
+    }).map((m) =>{
+        m.dataValues.user.dataValues.id
+    }).concat(contacts.filter((m) =>
+        m.dataValues.receipient.dataValues.id.toString() !== id
+    ).map((m) => m.dataValues.receipient.dataValues.id
+    )).concat(id);
+
+    const user = await users.findAll({
+        where: {
+            id: {
+                [Sequelize.Op.in]: userIds
+            }
+        },
+        attributes: ['id', 'firstname', 'phone', 'avatar']
+    });
+
+    const lastMessages = [];
+    for (const chats of user) {
+        const filteredMessages = contacts.filter((m) => (
+                m.dataValues.user.dataValues.id === id &&
+                m.dataValues.receipient.dataValues.id === chats.dataValues.id
+            ) || ( 
+                m.dataValues.user.dataValues.id === chats.dataValues.id &&
+                m.dataValues.receipient.dataValues.id === id
+            )
+        );
+
+        if (filteredMessages.length) {
+            lastMessages.push({
+                receiver: chats,
+                lastMessage: filteredMessages[0],
+                unreadcount: filteredMessages.filter((m => 
+                    m.dataValues.read === false && m.dataValues.receiver === id
+                )).length,
+                filteredMessages: filteredMessages,
+            });
+        }
+    }
+
+
+
+    return res.status(200).json({ message: "Successfully queried all users", chats: lastMessages });
     } catch(e) {
         return res.status(500).json({ error: "Database error while retrieving all chats" });
     }
--- Express/App/Middlewares/verifyJWT.js
+++ Express/App/Middlewares/verifyJWT.js
@@ -4,15 +4,15 @@ module.exports.verifyJWT = (req, res, next) => {
     const authHeader = req.headers['authorization'] ||  req.headers['Authorization'];
     try{
         const barerr  = authHeader.split(' ')[1];
-       if (!authHeader) return res.status(401).json({ messages: "Unauthorized token missing" });
+       if (!authHeader) return res.status(401).json({ error: "Unauthorized token missing" });
 
 
         jwt.verify(barerr || authHeader, process.env.ACCESS_TOKEN_SECRET, (err, decode) => {
-            if(err) return res.status(403).json({message: "invalid token"})
+            if(err) return res.status(403).json({error: "invalid token"})
             req.userId = decode.id;
             next();
         })
     }catch(e){
-        return res.status(401).json({message: "unable to decode token"})
+        return res.status(401).json({error: "unable to decode token"})
     }
 }
